56c37d98e08c3c9fde7ae8cb11222589f808c885
Ghawkes-eng <george@jubelbeer.com>
2026-05-05 09:50:37 +0100

    fix auth imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import { getServerSession } from 'next-auth'
 import { redirect } from 'next/navigation'
-import { authOptions } from './api/auth/[...nextauth]/route'
+import { authOptions } from '@/lib/auth'
 import SignInPage from './auth/signin/page'
 
 export default async function Home() {
